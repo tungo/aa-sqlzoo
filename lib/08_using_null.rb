90792bd234965581715820d72cb35400b59d3a3c
Tu Ngo <tungo8810@gmail.com>
2017-04-10 17:58:52 -0700

    Add using_null methods

--- lib/08_using_null.rb
+++ lib/08_using_null.rb
@@ -18,6 +18,12 @@ require_relative './sqlzoo.rb'
 def null_dept
   # List the teachers who have NULL for their department.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      teachers
+    WHERE
+      dept_id IS NULL
   SQL
 end
 
@@ -25,6 +31,13 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+    SELECT
+      t.name,
+      d.name
+    FROM
+      teachers t
+    LEFT JOIN
+      depts d ON d.id = t.dept_id
   SQL
 end
 
@@ -33,6 +46,13 @@ def all_depts_join
   # NB: you can avoid RIGHT OUTER JOIN (and just use LEFT) by swapping
   # the FROM and JOIN tables.
   execute(<<-SQL)
+    SELECT
+      t.name,
+      d.name
+    FROM
+      depts d
+    LEFT JOIN
+      teachers t ON t.dept_id = d.id
   SQL
 end
 
@@ -41,6 +61,7 @@ def teachers_and_mobiles
   # 444 2266' if no number is given. Show teacher name and mobile
   # #number or '07986 444 2266'
   execute(<<-SQL)
+
   SQL
 end
 
